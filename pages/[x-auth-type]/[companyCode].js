4ce6aeb330fb25971c5dd61be305a2609392b364
tazamaak07 <tazamaak07@gmail.com>
2024-10-11 14:53:47 +0700

    add /login

---
+++ pages/[x-auth-type]/[companyCode].js
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function RedirectPage() {
+    const router = useRouter();
+    const { "x-auth-type": authType, companyCode } = router.query;
+
+    useEffect(() => {
+        if (authType && companyCode) {
+            // Store x-auth-type and companyCode in localStorage
+            localStorage.setItem("x-auth-type", authType);
+            localStorage.setItem("companyCode", companyCode);
+
+            // Redirect to the home page
+            router.push("/");
+        }
+    }, [authType, companyCode, router]);
+
+    return <div>Redirecting...</div>;
+}
